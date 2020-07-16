f4454c9ef4ea6e6f48446b3bd3d93a4527a6cd62
NFerron24 <67276148+NFerron24@users.noreply.github.com>
2020-07-16 17:00:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let name = "Noah";
+console.log(name);
+//let if = 'Noah';
+//console.log (if);
+//lukt niet omdat 'if' een dedicated value is binnen javascript
+let som_1 = 4 + 4;
+console.log(som_1);
+let som_2 = "4" + "4";
+console.log(som_2);
+let som_3 = 4 - 5;
+console.log(som_3);
+let som_4 = 4 * 5;
+console.log(som_4);
+let som_5 = 2 % 5;
+console.log(som_5);
+let age = 28;
+console.log(age);
+let age_1 = "achtentwintig";
+console.log(age_1);
